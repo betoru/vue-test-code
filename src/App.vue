7e28a6040c0b529ce31c17317062d04ca028bc06
betoru <betoru@anyssign.com>
2022-08-25 12:12:00 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TestTest msg="You wanna Test?"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import TestTest from './components/TestTest.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TestTest
   }
 }
 </script>
---
+++ src/components/TestTest.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>짐은 미륵이니라</p>
+  </div>
+  <div>
+    <input v-model="testModel" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestTest',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      testModel:'',
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
